bc44807fea44c9eafd32d17b4745b21ab6e0b711
Amaury Martin <amaury.conde@hotmail.com>
2021-07-14 02:54:07 -0300

    Lint App component and index file  on frontend web

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,9 +1,3 @@
-import React from 'react';
+const App = (): JSX.Element => <h1>Hello, World!</h1>
 
-function App() {
-  return (
-    <h1>Hello, World!</h1>
-  );
-}
-
-export default App;
+export default App

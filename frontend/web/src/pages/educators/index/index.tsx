72809c405e9c561b23ee3f50496151c747cb1c97
Amaury Martin <amaury.conde@hotmail.com>
2021-07-18 02:17:20 -0300

    Improve EducatorsIndex component on frontend web

--- frontend/web/src/pages/educators/index/index.tsx
+++ frontend/web/src/pages/educators/index/index.tsx
@@ -1,10 +1,27 @@
+import { Link } from 'react-router-dom'
+
+import logoImg from '../../../assets/images/logo.svg'
+import backIcon from '../../../assets/images/icons/back.svg'
+
 import './styles.css'
 
 const EducatorsIndex = (): JSX.Element => {
   return (
-    <>
-      <h1>Educators Index</h1>
-    </>
+    <div id="educators-index" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={backIcon} alt="Go back" />
+          </Link>
+
+          <img src={logoImg} alt="Proffy" />
+        </div>
+
+        <div className="header-content">
+          <strong>These are the available educators</strong>
+        </div>
+      </header>
+    </div>
   )
 }
 

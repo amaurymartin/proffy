a9c7c2d2646c9c20cf342cbc185bcad23c18b0fa
Amaury Martin <amaury.conde@hotmail.com>
2021-07-19 02:37:27 -0300

    Improve EducatorsIndex component on frontend web

--- frontend/web/src/pages/educators/index/index.tsx
+++ frontend/web/src/pages/educators/index/index.tsx
@@ -1,4 +1,5 @@
 import Header from '../../../components/header'
+import whatsappIcon from '../../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
@@ -35,6 +36,41 @@ const EducatorsIndex = (): JSX.Element => {
           </div>
         </form>
       </Header>
+
+      <main>
+        <article className="educator">
+          <header>
+            <img
+              src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
+              alt="First Educator"
+            />
+            <div>
+              <strong>John Doe</strong>
+              <span>Math</span>
+            </div>
+          </header>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            <br />
+            <br />
+            Donec fermentum nibh ante, sed accumsan arcu cursus et. Donec
+            ultrices, tellus eget ultrices interdum, tellus metus interdum
+            sapien, eu fringilla magna lacus sit amet dolor.
+          </p>
+
+          <footer>
+            <p>
+              Per hour
+              <strong>USD 4.20</strong>
+            </p>
+            <button type="button">
+              <img src={whatsappIcon} alt="Whatsapp" />
+              Contact
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   )
 }

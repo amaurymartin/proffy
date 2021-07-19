39b2da26faa887dfba7ed5cf098dd5a35bb44b15
Amaury Martin <amaury.conde@hotmail.com>
2021-07-19 03:16:42 -0300

    Init server on backend

---
+++ backend/src/routes.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const routes = express.Router()
+
+// Healthcheck
+routes.get('/healthcheck', (_req, res) => {
+  res.status(204).send()
+})
+
+export default routes

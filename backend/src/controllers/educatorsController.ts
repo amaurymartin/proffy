b09b452ad6bdee0379eb2d1b65c7f54066a1483b
Amaury Martin <amaury.conde@hotmail.com>
2021-07-25 18:47:21 -0300

    Add EducatorsController on backend

---
+++ backend/src/controllers/educatorsController.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from 'express';
+
+import connection from '../db/connection';
+
+import educatorRepository from '../repositories/educator/educatorRepository';
+import classRepository from '../repositories/classRepository';
+import scheduleRepository from '../repositories/educator/scheduleRepository';
+
+type EducatorParams = {
+  educator: {
+    name: string;
+    avatar: string;
+    email: string;
+    whatsapp: string;
+    bio: string;
+    classes: [
+      {
+        subject: string;
+        description: string;
+        price: number;
+      }
+    ];
+    schedules: [
+      {
+        weekDay: number;
+        startsAt: string;
+        endsAt: string;
+      }
+    ];
+  };
+};
+
+class EducatorsController {
+  static async create(req: Request, res: Response) {
+    const { educator }: EducatorParams = req.body;
+
+    const trx = await connection.transaction();
+
+    try {
+      const educatorId = await educatorRepository.create(educator, trx);
+      await classRepository.create(educatorId, educator.classes, trx);
+      await scheduleRepository.create(educatorId, educator.schedules, trx);
+
+      await trx.commit();
+
+      return res.status(201).json(educatorId);
+    } catch (error) {
+      await trx.rollback();
+
+      return res.status(422).json({ error: 'Error on creating educator. Check your data!' });
+    }
+  }
+}
+
+export default EducatorsController;
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 
+import EducatorsController from './controllers/educatorsController';
+
 const routes = express.Router();
 
 // Healthcheck
 routes.get('/healthcheck', (_req, res) => {
   res.status(204).send();
 });
+
+routes.post('/educators', EducatorsController.create);
 
 export default routes;

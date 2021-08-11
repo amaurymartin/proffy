11f0d71687f8059700314274d2a618ab182b9e82
Amaury Martin <amaury.conde@hotmail.com>
2021-08-11 02:42:37 -0300

    Improve educatorsNew component on frontend web

--- frontend/web/src/pages/educators/new/index.tsx
+++ frontend/web/src/pages/educators/new/index.tsx
@@ -1,5 +1,7 @@
 import Header from '../../../components/header'
 
+import warningIcon from '../../../assets/images/icons/warning.svg'
+
 import './styles.css'
 
 const EducatorsNew = (): JSX.Element => {
@@ -9,6 +11,65 @@ const EducatorsNew = (): JSX.Element => {
         title="It's amazing that you want to teach classes"
         subtitle="Fill the form to start teaching"
       />
+
+      <main>
+        <fieldset>
+          <legend>Personal data</legend>
+
+          <div className="input-block">
+            <label htmlFor="name">
+              Full Name
+              <input type="text" id="name" />
+            </label>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="avatar">
+              Avatar
+              <input type="text" id="avatar" />
+            </label>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="name">
+              Whatsapp
+              <input type="text" id="whatsapp" />
+            </label>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>Classes</legend>
+
+          <div className="input-block">
+            <label htmlFor="subject">
+              Subject
+              <input type="text" id="subject" />
+            </label>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="price">
+              Price per hour
+              <input type="number" id="price" step=".01" />
+            </label>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>Schedule</legend>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Warning" />
+            Warning! <br />
+            Fill all fields
+          </p>
+
+          <button type="submit">Submit</button>
+        </footer>
+      </main>
     </div>
   )
 }

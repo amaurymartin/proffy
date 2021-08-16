6a56dc47df1a1a77bd33b3ac47a156df51c60720
Amaury Martin <amaury.conde@hotmail.com>
2021-08-16 01:36:10 -0300

    Add input component on frontend web

---
+++ frontend/web/src/components/input/index.tsx
@@ -0,0 +1,35 @@
+import { InputHTMLAttributes } from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.css'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string
+  label: string
+  type: string
+  placeholder?: string
+}
+
+const Input: React.FC<InputProps> = ({ name, label, type, placeholder }) => {
+  return (
+    <div className="input-block">
+      <label htmlFor={name}>
+        {label}
+        <input type={type} name={name} id={name} placeholder={placeholder} />
+      </label>
+    </div>
+  )
+}
+
+Input.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+}
+
+Input.defaultProps = {
+  placeholder: 'Type here',
+}
+
+export default Input

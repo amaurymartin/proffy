fc914eb107ad2626a705b383625fac8d4a250b1a
Amaury Martin <amaury.conde@hotmail.com>
2021-08-19 03:29:51 -0300

    Init educatorsNew component integration with API on frontend web

--- frontend/web/src/pages/educators/new/index.tsx
+++ frontend/web/src/pages/educators/new/index.tsx
@@ -1,12 +1,109 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
 import Header from '../../../components/header'
 import Input from '../../../components/input'
 import Select from '../../../components/select'
+
+import api from '../../../services/api'
 
 import warningIcon from '../../../assets/images/icons/warning.svg'
 
 import './styles.css'
 
+type Schedule = {
+  weekDay: number
+  startsAt: string
+  endsAt: string
+}
+
 const EducatorsNew = (): JSX.Element => {
+  const history = useHistory()
+
+  const [schedule, setSchedule] = useState<Schedule[]>([
+    { weekDay: 0, startsAt: '', endsAt: '' },
+  ])
+
+  const [formData, setFormData] = useState({
+    name: '',
+    avatar: '',
+    whatsapp: '',
+    bio: '',
+    subject: '',
+    price: 0.0,
+  })
+
+  function addSchedule() {
+    setSchedule([
+      ...schedule,
+      {
+        weekDay: 0,
+        startsAt: '',
+        endsAt: '',
+      },
+    ])
+  }
+
+  function fillForm(event: ChangeEvent<HTMLElement>) {
+    if (event.target.nodeName === 'INPUT') {
+      const inputEvent = event as ChangeEvent<HTMLInputElement>
+      const { name, value } = inputEvent.target
+      setFormData({ ...formData, [name]: value })
+    } else if (event.target.nodeName === 'TEXTAREA') {
+      const selectEvent = event as ChangeEvent<HTMLTextAreaElement>
+      const { name, value } = selectEvent.target
+      setFormData({ ...formData, [name]: value })
+    } else if (event.target.nodeName === 'SELECT') {
+      const selectEvent = event as ChangeEvent<HTMLSelectElement>
+      const { name, value } = selectEvent.target
+      setFormData({ ...formData, [name]: value })
+    }
+  }
+
+  function fillScheduleData(event: ChangeEvent<HTMLElement>, position: number) {
+    const selectEvent = event as ChangeEvent<HTMLSelectElement>
+    const { name, value } = selectEvent.target
+    const newSchedule = schedule.map((item, index) => {
+      if (index === position) return { ...item, [name]: value }
+
+      return item
+    })
+
+    setSchedule(newSchedule)
+  }
+
+  async function createEducator(event: FormEvent) {
+    event.preventDefault()
+    const { name, avatar, whatsapp, bio, subject, price } = formData
+
+    const payload = {
+      educator: {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        class: {
+          subject,
+          price,
+        },
+        schedule,
+      },
+    }
+
+    console.log('payload', payload)
+    await api
+      .post('educators', payload)
+      .then(() => {
+        // eslint-disable-next-line no-alert
+        alert('Educator created')
+        history.push('/')
+      })
+      .catch(() => {
+        // eslint-disable-next-line no-alert
+        alert('Error on creating educator. Check your data and try again')
+      })
+  }
+
   return (
     <div id="educators-new" className="container">
       <Header
@@ -15,93 +112,135 @@ const EducatorsNew = (): JSX.Element => {
       />
 
       <main>
-        <fieldset>
-          <legend>Personal data</legend>
+        <form onSubmit={createEducator}>
+          <fieldset>
+            <legend>Personal data</legend>
 
-          <Input name="name" label="Full Name" type="text" />
-          <Input name="avatar" label="Avatar" type="text" />
-          <Input name="whatsapp" label="Whatsapp" type="text" />
+            <Input
+              name="name"
+              label="Full Name"
+              type="text"
+              value={formData.name}
+              onChange={fillForm}
+            />
+            <Input
+              name="avatar"
+              label="Avatar"
+              type="text"
+              value={formData.avatar}
+              onChange={fillForm}
+            />
+            <Input
+              name="whatsapp"
+              label="Whatsapp"
+              type="text"
+              value={formData.whatsapp}
+              onChange={fillForm}
+            />
 
-          <div className="text-area-block">
-            <label htmlFor="bio">
-              Bio
-              <textarea id="bio" />
-            </label>
-          </div>
-        </fieldset>
+            <div className="text-area-block">
+              <label htmlFor="bio">
+                Bio
+                <textarea name="bio" value={formData.bio} onChange={fillForm} />
+              </label>
+            </div>
+          </fieldset>
 
-        <fieldset>
-          <legend>Classes</legend>
+          <fieldset>
+            <legend>Classes</legend>
 
-          <Select
-            name="subject"
-            label="Subject"
-            placeholder="Select your subject"
-            options={[
-              { value: 'ENGLISH', label: 'English' },
-              { value: 'LITERATURE', label: 'Literature' },
-              { value: 'SPEECH', label: 'Speech' },
-              { value: 'WRITING', label: 'Writing' },
-              { value: 'FOREIGN', label: 'Foreign Language' },
-              { value: 'GEOGRAPHY', label: 'Geography' },
-              { value: 'HISTORY', label: 'History' },
-              { value: 'ECONOMICS', label: 'Economics' },
-              { value: 'MATH', label: 'Math' },
-              { value: 'ALGEBRA', label: 'Algebra' },
-              { value: 'GEOMETRY', label: 'Geometry' },
-              { value: 'STATISTICS', label: 'Statistics' },
-              { value: 'TRIGONOMETRY', label: 'Trigonometry' },
-              { value: 'SCIENCE', label: 'Science' },
-              { value: 'BIOLOGY', label: 'Biology' },
-              { value: 'CHEMISTRY', label: 'Chemistry' },
-              { value: 'PHYSICS', label: 'Physics' },
-            ]}
-          />
-
-          <div className="input-block">
-            <label htmlFor="price">
-              Price per hour
-              <input type="number" id="price" step=".01" />
-            </label>
-          </div>
-        </fieldset>
-
-        <fieldset>
-          <legend>
-            Schedule
-            <button type="button">+ Add schedule</button>
-          </legend>
-
-          <div className="schedule">
             <Select
-              name="day"
-              label="Week Day"
-              placeholder="Select the best day for you"
+              name="subject"
+              label="Subject"
+              placeholder="Select your subject"
+              value={formData.subject}
+              onChange={fillForm}
               options={[
-                { value: '0', label: 'Sunday' },
-                { value: '1', label: 'Monday' },
-                { value: '2', label: 'Tuesday' },
-                { value: '3', label: 'Wednesday' },
-                { value: '4', label: 'Thursday' },
-                { value: '5', label: 'Friday' },
-                { value: '6', label: 'Saturday' },
+                { value: 'ENGLISH', label: 'English' },
+                { value: 'LITERATURE', label: 'Literature' },
+                { value: 'SPEECH', label: 'Speech' },
+                { value: 'WRITING', label: 'Writing' },
+                { value: 'FOREIGN', label: 'Foreign Language' },
+                { value: 'GEOGRAPHY', label: 'Geography' },
+                { value: 'HISTORY', label: 'History' },
+                { value: 'ECONOMICS', label: 'Economics' },
+                { value: 'MATH', label: 'Math' },
+                { value: 'ALGEBRA', label: 'Algebra' },
+                { value: 'GEOMETRY', label: 'Geometry' },
+                { value: 'STATISTICS', label: 'Statistics' },
+                { value: 'TRIGONOMETRY', label: 'Trigonometry' },
+                { value: 'SCIENCE', label: 'Science' },
+                { value: 'BIOLOGY', label: 'Biology' },
+                { value: 'CHEMISTRY', label: 'Chemistry' },
+                { value: 'PHYSICS', label: 'Physics' },
               ]}
             />
 
-            <Input name="start-time" label="Start" type="time" />
-            <Input name="end-time" label="End" type="time" />
-          </div>
-        </fieldset>
+            <div className="input-block">
+              <label htmlFor="price">
+                Price per hour
+                <input
+                  name="price"
+                  type="number"
+                  step=".01"
+                  value={formData.price}
+                  onChange={fillForm}
+                />
+              </label>
+            </div>
+          </fieldset>
 
-        <footer>
-          <p>
-            <img src={warningIcon} alt="Warning" />
-            Warning! <br />
-            Fill all fields
-          </p>
+          <fieldset>
+            <legend>
+              Schedule
+              <button type="button" onClick={() => addSchedule}>
+                + Add schedule
+              </button>
+            </legend>
 
-          <button type="submit">Submit</button>
-        </footer>
+            {schedule.map((item, index) => (
+              <div key={item.weekDay} className="schedule">
+                <Select
+                  name="weekDay"
+                  label="Week Day"
+                  placeholder="Select the best day for you"
+                  options={[
+                    { value: '0', label: 'Sunday' },
+                    { value: '1', label: 'Monday' },
+                    { value: '2', label: 'Tuesday' },
+                    { value: '3', label: 'Wednesday' },
+                    { value: '4', label: 'Thursday' },
+                    { value: '5', label: 'Friday' },
+                    { value: '6', label: 'Saturday' },
+                  ]}
+                  onChange={(event) => fillScheduleData(event, index)}
+                />
+
+                <Input
+                  name="startsAt"
+                  label="Start"
+                  type="time"
+                  onChange={() => undefined}
+                />
+                <Input
+                  name="endsAt"
+                  label="End"
+                  type="time"
+                  onChange={() => undefined}
+                />
+              </div>
+            ))}
+          </fieldset>
+          <footer>
+            <p>
+              <img src={warningIcon} alt="Warning" />
+              Warning! <br />
+              Fill all fields
+            </p>
+
+            <button type="submit">Submit</button>
+          </footer>
+        </form>
       </main>
     </div>
   )

894caaed633e91270671f9036962a8002e17312a
Amaury Martin <amaury.conde@hotmail.com>
2021-08-21 04:42:03 -0300

    Integrate educatorsIndex component with api on frontend web

--- frontend/web/src/pages/educators/index/index.tsx
+++ frontend/web/src/pages/educators/index/index.tsx
@@ -1,20 +1,96 @@
+import { ChangeEvent, FormEvent, useState } from 'react'
+
 import Header from '../../../components/header'
 import Select from '../../../components/select'
 import Input from '../../../components/input'
 import Educator from '../../../components/educator'
 
+import api from '../../../services/api'
+
 import './styles.css'
 
+type ClassIndexResponse = {
+  classes: Class[]
+}
+
+type Class = {
+  key: string
+  subject: string
+  description: string
+  price: string
+  educator: {
+    key: string
+  }
+}
+
 const EducatorsIndex = (): JSX.Element => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [classes, setClasses] = useState<Class[]>([])
+  const [searchParams, setSearchParams] = useState({
+    subject: '',
+    weekDay: '',
+    time: '',
+  })
+
+  function addQueryParam(event: ChangeEvent<HTMLElement>) {
+    if (event.target.nodeName === 'SELECT') {
+      const selectEvent = event as ChangeEvent<HTMLSelectElement>
+      const { name, value } = selectEvent.target
+      setSearchParams(Object.assign(searchParams, { [name]: value }))
+    } else if (event.target.nodeName === 'INPUT') {
+      const inputEvent = event as ChangeEvent<HTMLInputElement>
+      const { name, value } = inputEvent.target
+      setSearchParams(Object.assign(searchParams, { [name]: value }))
+    }
+  }
+
+  function mountQueryParams() {
+    let queryParams = {}
+
+    if (searchParams.subject !== '')
+      queryParams = { subject: searchParams.subject }
+    if (searchParams.weekDay !== '') {
+      queryParams = Object.assign(queryParams, {
+        weekDay: searchParams.weekDay,
+      })
+    }
+    if (searchParams.time !== '')
+      queryParams = Object.assign(queryParams, { time: searchParams.time })
+
+    if (queryParams === {}) return {}
+
+    return { params: queryParams }
+  }
+
+  function searchClasses(event: FormEvent) {
+    event.preventDefault()
+
+    // eslint-disable-next-line no-console
+    console.log('mountQueryParams', mountQueryParams())
+    api
+      .get<ClassIndexResponse>('classes', mountQueryParams())
+      .then((response) => {
+        setClasses(response.data.classes)
+      })
+      .catch(() => {
+        // eslint-disable-next-line no-alert
+        alert('Error on searching classes. Try again')
+      })
+
+    // eslint-disable-next-line no-console
+    console.log('classes', classes)
+  }
+
   return (
     <div id="educators-index" className="container">
       <Header title="These are the available educators">
-        <form id="educators-search">
+        <form id="educators-search" onSubmit={searchClasses}>
           <Select
             name="subject"
             label="Subject"
             placeholder="Select your subject"
             options={[
+              { value: '', label: 'Any' },
               { value: 'ENGLISH', label: 'English' },
               { value: 'LITERATURE', label: 'Literature' },
               { value: 'SPEECH', label: 'Speech' },
@@ -33,14 +109,15 @@ const EducatorsIndex = (): JSX.Element => {
               { value: 'CHEMISTRY', label: 'Chemistry' },
               { value: 'PHYSICS', label: 'Physics' },
             ]}
-            onChange={() => undefined}
+            onChange={(event) => addQueryParam(event)}
           />
 
           <Select
-            name="day"
+            name="weekDay"
             label="Week Day"
             placeholder="Select the best day for you"
             options={[
+              { value: '', label: 'Any' },
               { value: '0', label: 'Sunday' },
               { value: '1', label: 'Monday' },
               { value: '2', label: 'Tuesday' },
@@ -49,15 +126,17 @@ const EducatorsIndex = (): JSX.Element => {
               { value: '5', label: 'Friday' },
               { value: '6', label: 'Saturday' },
             ]}
-            onChange={() => undefined}
+            onChange={(event) => addQueryParam(event)}
           />
 
           <Input
             name="time"
             label="Time"
             type="time"
-            onChange={() => undefined}
+            onChange={(event) => addQueryParam(event)}
           />
+
+          <button type="submit">Serch</button>
         </form>
       </Header>
 

bf0e3b52f05276f66bd14e549c11a06a6848aab1
Amaury Martin <amaury.conde@hotmail.com>
2021-08-23 01:31:55 -0300

    Fix educator component to get real info on frontend web

--- frontend/web/src/components/educator/index.tsx
+++ frontend/web/src/components/educator/index.tsx
@@ -1,42 +1,97 @@
+import PropTypes from 'prop-types'
+
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import api from '../../services/api'
 
 import './styles.css'
 
-const Educator = (): JSX.Element => {
+type EducatorProps = {
+  educator: {
+    key: string
+    avatar: string
+    name: string
+    bio: string
+    email: string
+    whatsapp: string
+  }
+  klassKey: string
+  klassSubject: string
+  klassPrice: number
+}
+
+const Educator: React.FC<EducatorProps> = ({
+  educator,
+  klassKey,
+  klassSubject,
+  klassPrice,
+}) => {
+  async function scheduleClass() {
+    // TODO: Should this const be an env?
+    const acceptedClassStatus = 1
+
+    const payload = {
+      klass: {
+        key: 'status',
+        value: acceptedClassStatus,
+      },
+    }
+
+    await api
+      .patch(`classes/${klassKey}`, payload)
+      .then(() => {
+        // eslint-disable-next-line no-alert
+        alert('Class schedule successfully')
+      })
+      .catch(() => {
+        // eslint-disable-next-line no-alert
+        alert('Error on scheduling the class. Try again later')
+      })
+  }
+
   return (
     <article className="educator">
       <header>
-        <img
-          src="https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
-          alt="First Educator"
-        />
+        <img src={educator.avatar} alt={educator.avatar} />
         <div>
-          <strong>John Doe</strong>
-          <span>Math</span>
+          <strong>{educator.name}</strong>
+          <span>{klassSubject}</span>
         </div>
       </header>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        <br />
-        <br />
-        Donec fermentum nibh ante, sed accumsan arcu cursus et. Donec ultrices,
-        tellus eget ultrices interdum, tellus metus interdum sapien, eu
-        fringilla magna lacus sit amet dolor.
-      </p>
+      <p>{educator.bio}</p>
 
       <footer>
         <p>
           Per hour
-          <strong>USD 4.20</strong>
+          <strong>USD {klassPrice}</strong>
         </p>
-        <button type="button">
+        <a
+          href={`https://wa.me/55${educator.whatsapp}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={scheduleClass}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Contact
-        </button>
+        </a>
       </footer>
     </article>
   )
+}
+
+Educator.propTypes = {
+  educator: PropTypes.shape({
+    key: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    bio: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    whatsapp: PropTypes.string.isRequired,
+  }).isRequired,
+  klassKey: PropTypes.string.isRequired,
+  klassSubject: PropTypes.string.isRequired,
+  klassPrice: PropTypes.number.isRequired,
 }
 
 export default Educator

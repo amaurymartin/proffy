eddb43ea17f31bd61bacacb8adc9386e66eeb9cb
Amaury Martin <amaury.conde@hotmail.com>
2021-08-25 02:41:02 -0300

    Add google fonts on frontend mobile

--- frontend/mobile/App.tsx
+++ frontend/mobile/App.tsx
@@ -2,10 +2,22 @@
 import React from 'react'
 
 import { StatusBar } from 'expo-status-bar'
+import AppLoading from 'expo-app-loading';
+
+// eslint-disable-next-line camelcase
+import { useFonts, Archivo_400Regular, Archivo_700Bold } from '@expo-google-fonts/archivo'
+// eslint-disable-next-line camelcase
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 
 import Home from './src/pages/home'
 
 export default function App(): JSX.Element {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular, Archivo_700Bold, Poppins_400Regular, Poppins_600SemiBold
+  })
+
+  if(!fontsLoaded) return <AppLoading />
+
   return (
     <>
       <Home />

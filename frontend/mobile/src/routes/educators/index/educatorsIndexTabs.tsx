f07edb0424eb463a8585362f995dbd6e392a49ff
Amaury Martin <amaury.conde@hotmail.com>
2021-08-27 02:05:01 -0300

    Fix educatorsIndexTabs routing on frontend mobile

--- frontend/mobile/src/routes/educators/index/educatorsIndexTabs.tsx
+++ frontend/mobile/src/routes/educators/index/educatorsIndexTabs.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
+import { Ionicons } from '@expo/vector-icons'
+
 import EducatorsIndex from '../../../pages/educators/index'
 import EducatorsIndexFavorites from '../../../pages/educators/index/favorites'
 
@@ -9,11 +11,52 @@ const { Navigator, Screen } = createBottomTabNavigator()
 
 function EducatorsIndexTabs(): JSX.Element {
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="EducatorsIndex" component={EducatorsIndex} />
+    <Navigator
+      screenOptions={({ route }) => ({
+        // eslint-disable-next-line react/prop-types
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons
+            name={route.name === 'EducatorsIndex' ? 'ios-easel' : 'ios-heart'}
+            color={color}
+            size={size}
+          />
+        ),
+        headerShown: false,
+        activeBackgroundColor: '#EBEBF5',
+        activeTintColor: '#32264D',
+        inactiveBackgroundColor: '#FAFAFC',
+        inactiveTintColor: '#C1BCCC',
+        style: {
+          elevation: 0,
+          shadowOpacity: 0,
+          height: 64,
+        },
+        tabStyle: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+        iconStyle: {
+          flex: 0,
+          width: 20,
+          height: 20,
+        },
+        labelStyle: {
+          fontFamily: 'Archivo_700Bold',
+          fontSize: 13,
+          marginLeft: 16,
+        },
+      })}
+    >
+      <Screen
+        name="EducatorsIndex"
+        component={EducatorsIndex}
+        options={{ tabBarLabel: 'All' }}
+      />
       <Screen
         name="EducatorsIndexFavorites"
         component={EducatorsIndexFavorites}
+        options={{ tabBarLabel: 'Favorites' }}
       />
     </Navigator>
   )

66e0a96b43946e2c5253a15c7b3335ec7a9f2ba0
Amaury Martin <amaury.conde@hotmail.com>
2021-08-30 03:03:25 -0300

    Add educator component in educatorsIndex page on frontend mobile

--- frontend/mobile/src/pages/educators/index/index.tsx
+++ frontend/mobile/src/pages/educators/index/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import { View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 
 import Header from '../../../components/header'
+import Educator from '../../../components/educator'
 
 import styles from './styles'
 
@@ -10,6 +11,20 @@ export default function EducatorsIndex(): JSX.Element {
   return (
     <View style={styles.container}>
       <Header title="Educators" />
+
+      <ScrollView
+        style={styles.educators}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <Educator />
+        <Educator />
+        <Educator />
+        <Educator />
+        <Educator />
+      </ScrollView>
     </View>
   )
 }

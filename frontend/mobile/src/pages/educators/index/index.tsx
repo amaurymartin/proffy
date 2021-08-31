d30263a7659a570ebf0d883c4ed27924ec2d1c26
Amaury Martin <amaury.conde@hotmail.com>
2021-08-31 00:55:13 -0300

    Add filters on educatorsIndex page on frontend mobile

--- frontend/mobile/src/pages/educators/index/index.tsx
+++ frontend/mobile/src/pages/educators/index/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ScrollView, View } from 'react-native'
+import { ScrollView, Text, TextInput, View } from 'react-native'
+
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
+
+import { Feather } from '@expo/vector-icons'
 
 import Header from '../../../components/header'
 import Educator from '../../../components/educator'
@@ -8,9 +12,54 @@ import Educator from '../../../components/educator'
 import styles from './styles'
 
 export default function EducatorsIndex(): JSX.Element {
+  const [isFiltersVisible, setIsFiltersVisible] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Header title="Educators" />
+      <Header
+        title="Educators"
+        icon={
+          <BorderlessButton
+            onPress={() => setIsFiltersVisible(!isFiltersVisible)}
+          >
+            <Feather name="filter" size={20} color="#FFF" />
+          </BorderlessButton>
+        }
+      >
+        {isFiltersVisible && (
+          <View style={styles.search}>
+            <Text style={styles.label}>Subject</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Select your subject"
+              placeholderTextColor="#C1BCCC"
+            />
+
+            <View style={styles.schedule}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Week Day</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Select the best day"
+                  placeholderTextColor="#C1BCCC"
+                />
+              </View>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Time</Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Select the best time"
+                  placeholderTextColor="#C1BCCC"
+                />
+              </View>
+            </View>
+
+            <RectButton style={styles.button} onPress={() => undefined}>
+              <Text style={styles.buttonText}>Search</Text>
+            </RectButton>
+          </View>
+        )}
+      </Header>
 
       <ScrollView
         style={styles.educators}

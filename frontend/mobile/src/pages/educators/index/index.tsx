b3ff041d5c98be15a3d02b1d7c5253b64e2e3a09
Amaury Martin <amaury.conde@hotmail.com>
2021-08-31 02:10:33 -0300

    Connect api with educatorsIndex page on frontend mobile

--- frontend/mobile/src/pages/educators/index/index.tsx
+++ frontend/mobile/src/pages/educators/index/index.tsx
@@ -1,6 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { ScrollView, Text, TextInput, View } from 'react-native'
+import {
+  NativeSyntheticEvent,
+  ScrollView,
+  Text,
+  TextInput,
+  TextInputChangeEventData,
+  View,
+} from 'react-native'
 
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
 
@@ -10,9 +17,79 @@ import Header from '../../../components/header'
 import Educator from '../../../components/educator'
 
 import styles from './styles'
+import api from '../../../services/api'
+
+type ClassIndexResponse = {
+  classes: Class[]
+}
+
+type Class = {
+  key: string
+  subject: string
+  description: string
+  price: number
+  status: number
+  educator: {
+    key: string
+    avatar: string
+    name: string
+    bio: string
+    email: string
+    whatsapp: string
+  }
+}
 
 export default function EducatorsIndex(): JSX.Element {
   const [isFiltersVisible, setIsFiltersVisible] = useState(false)
+  const [subject, setSubject] = useState('')
+  const [weekDay, setWeekDay] = useState('')
+  const [time, setTime] = useState('')
+  const [classes, setClasses] = useState<Class[]>([])
+
+  useEffect(() => {
+    async function getClasses() {
+      await api
+        .get<ClassIndexResponse>('classes')
+        .then((response) => {
+          setClasses(response.data.classes)
+        })
+        .catch(() => {
+          // eslint-disable-next-line no-alert
+          alert('Error on searching classes. Try again')
+        })
+    }
+
+    getClasses()
+  }, [])
+
+  // TODO: improve this method
+  function formatTime(unformatedTime: string) {
+    setTime(unformatedTime.toUpperCase())
+  }
+
+  function mountQueryParams() {
+    let queryParams = {}
+
+    if (subject !== '') queryParams = { subject }
+    if (weekDay !== '') queryParams = Object.assign(queryParams, { weekDay })
+    if (time !== '') queryParams = Object.assign(queryParams, { time })
+
+    return { params: queryParams }
+  }
+
+  async function searchClasses() {
+    setIsFiltersVisible(!isFiltersVisible)
+
+    await api
+      .get<ClassIndexResponse>('classes', mountQueryParams())
+      .then((response) => {
+        setClasses(response.data.classes)
+      })
+      .catch(() => {
+        // eslint-disable-next-line no-alert
+        alert('Error on searching classes. Try again')
+      })
+  }
 
   return (
     <View style={styles.container}>
@@ -33,6 +110,7 @@ export default function EducatorsIndex(): JSX.Element {
               style={styles.input}
               placeholder="Select your subject"
               placeholderTextColor="#C1BCCC"
+              onChangeText={(text) => setSubject(String(text).toUpperCase())}
             />
 
             <View style={styles.schedule}>
@@ -42,6 +120,9 @@ export default function EducatorsIndex(): JSX.Element {
                   style={styles.input}
                   placeholder="Select the best day"
                   placeholderTextColor="#C1BCCC"
+                  onChangeText={(text) =>
+                    setWeekDay(String(text).toUpperCase())
+                  }
                 />
               </View>
               <View style={styles.inputBlock}>
@@ -50,11 +131,14 @@ export default function EducatorsIndex(): JSX.Element {
                   style={styles.input}
                   placeholder="Select the best time"
                   placeholderTextColor="#C1BCCC"
+                  defaultValue=""
+                  value={time}
+                  onChangeText={(text) => formatTime(text)}
                 />
               </View>
             </View>
 
-            <RectButton style={styles.button} onPress={() => undefined}>
+            <RectButton style={styles.button} onPress={() => searchClasses()}>
               <Text style={styles.buttonText}>Search</Text>
             </RectButton>
           </View>
@@ -68,11 +152,14 @@ export default function EducatorsIndex(): JSX.Element {
           paddingBottom: 16,
         }}
       >
-        <Educator />
-        <Educator />
-        <Educator />
-        <Educator />
-        <Educator />
+        {classes.map((klass) => (
+          <Educator
+            key={klass.educator.key}
+            educator={klass.educator}
+            klassSubject={klass.subject}
+            klassPrice={klass.price}
+          />
+        ))}
       </ScrollView>
     </View>
   )
